4054751465024cef9fdb2d337a1ac277a319cdde
Sean McBride <spmcbride1201@gmail.com>
2019-01-31 18:15:44 -0500

    chore: adding instructions for other problems

---
+++ problem3.c
@@ -0,0 +1,4 @@
+// Implement the deleteMin operation for priority queue with only a single(normal, non-priority) queue in
+// which to store data. You are allowed to declare int variables, but not arrays, vectors or any other data
+// structures. You can assume the Queue class provides the following methods: void enqueue(int), int
+// dequeue(), int size().
---
+++ problem5.c
@@ -0,0 +1,6 @@
+// Write an algorithm that takes a binary tree T(at most N nodes) as an input and computes the number of
+// nodes at each depth. The algorithm should store the output in an array D[0:N-1], where your algorithm
+// should store the number of nodes at depth k in D[k]
+
+// Input is a binary tree
+// Output is an array indexed by level with a value equal to the number of nodes at that level
